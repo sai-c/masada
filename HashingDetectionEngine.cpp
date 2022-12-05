9be16db0bd971571183406e0024d88e9a7964db5
Shivansh Srivastava <ss5945@columbia.edu>
2022-12-05 12:39:18 -0500

    Downloads the database of MD5 virus hashes before scanning for matches

--- HashingDetectionEngine.cpp
+++ HashingDetectionEngine.cpp
@@ -3,33 +3,31 @@
 #include <iostream>
 #include <string.h>
 #include <fstream>
-#include <cstdio>
 #include <memory>
 #include <stdexcept>
 #include <array>
-using namespace std;
 
-int TOTAL_WORD_LISTS = 449;
+const int TOTAL_WORD_LISTS = 449;
 
-string toFiveDigits(int i){
+std::string toFiveDigits(int i){
     if (i > 9999){
-        return "" + to_string(i);
+        return "" + std::to_string(i);
     } else if (i > 999){
-        return "0" + to_string(i);
+        return "0" + std::to_string(i);
     } else if (i > 99){
-        return "00" + to_string(i);
+        return "00" + std::to_string(i);
     } else if (i > 9){
-        return "000" + to_string(i);
+        return "000" + std::to_string(i);
     } else {
-        return "0000" + to_string(i);
+        return "0000" + std::to_string(i);
     }
 }
 
-string exec(const char* cmd) {
-    array<char, 128> buffer;
-    string result;
-    shared_ptr<FILE> pipe(popen(cmd, "r"), pclose);
-    if (!pipe) throw runtime_error("popen() failed!");
+std::string exec(const char* cmd) {
+    std::array<char, 128> buffer;
+    std::string result;
+    std::shared_ptr<FILE> pipe(popen(cmd, "r"), pclose);
+    if (!pipe) throw std::runtime_error("popen() failed!");
     while (!feof(pipe.get())) {
         if (fgets(buffer.data(), 128, pipe.get()) != nullptr)
             result += buffer.data();
@@ -39,11 +37,11 @@ string exec(const char* cmd) {
 
 void download(){
     for (int i = 0; i <= TOTAL_WORD_LISTS; i++) {
-        string num = toFiveDigits(i);
-        ifstream ifile;
+        std::string num = toFiveDigits(i);
+        std::ifstream ifile;
         ifile.open("VirusList/VirusShare_" + num + ".md5");
         if(!ifile){
-            cout << "Downloading Word Lists " + num + "/00449 (" + to_string((100.0/TOTAL_WORD_LISTS)*i) + "%)" << '\r' << flush;
+            std::cout << "Downloading Word Lists " + num + "/00449 (" + std::to_string((100.0/TOTAL_WORD_LISTS)*i) + "%)" << '\r' << std::flush;
             system(("wget -q -P VirusList https://virusshare.com/hashfiles/VirusShare_" + num + ".md5").c_str());
         }
         ifile.close();
@@ -51,21 +49,21 @@ void download(){
     printf("\n\033[1;32mDownload finished!\n");
 }
 
-int scan(string file) {
-    ifstream ifile;
+int scan(std::string file) {
+    std::ifstream ifile;
     ifile.open(file);
     if (ifile) {
         int scanned = 0;
-        string md5sum = exec(("md5sum " + file).c_str()).substr(0, 32);
-        cout << "MD5 hash: " << md5sum << endl;
+        std::string md5sum = exec(("md5sum " + file).c_str()).substr(0, 32);
+        std::cout << "MD5 hash: " << md5sum << std::endl;
         for (int i = 0; i <= TOTAL_WORD_LISTS; i++) {
-            string num = toFiveDigits(i);
-            ifstream wfile;
+            std::string num = toFiveDigits(i);
+            std::ifstream wfile;
             wfile.open("VirusList/VirusShare_" + num + ".md5");
             if (wfile.is_open()) {
-                string md5_hash;
-                while (getline(wfile, md5_hash)){
-                    cout << "Scanning file... " + to_string(scanned) + "/35258368 (" + to_string((100.0/35258368)*scanned) + "%)" << '\r' << flush;
+                std::string md5_hash;
+                while (std::getline(wfile, md5_hash)){
+                    std::cout << "Scanning file... " + std::to_string(scanned) + "/35258368 (" + std::to_string((100.0/35258368)*scanned) + "%)" << '\r' << std::flush;
                     if (md5sum == md5_hash) return 2;
                     scanned++;
                 }
@@ -83,16 +81,18 @@ int scan(string file) {
 bool HashingDetectionEngine::checkFile(std::string filePath) {
 
     hashDefinitions_.checkSignature("test");
+    
+    download();
     int res = scan(filePath);
     switch(res) {
         case 0:
-            cout << "File does not exist. Please specify a valid file and try again." << endl;
+            std::cout << "File does not exist. Please specify a valid file and try again." << std::endl;
             return false;
         case 1:
-            cout << "File was NOT found on the virus list. Opening this file may be safe." << endl;
+            std::cout << "File was NOT found on the virus list. Opening this file may be safe." << std::endl;
             return false;
         case 2:
-            cout << "File WAS found on the virus list. Please delete the file." << endl;
+            std::cout << "File WAS found on the virus list. Please delete the file." << std::endl;
             return true;
     }
     return false;
